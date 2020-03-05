77f08f1d0f3ea0eac188036e247fbd8fe65d4e47
Pro_Snape <rkdrns4747@users.noreply.github.com>
2020-03-05 20:28:37 +0900

    Bug Fix. For 1.12.2

---
+++ src/main/java/com/janmaki/mqrimo/invisible_armor/ClickEvent.java
@@ -0,0 +1,86 @@
+package com.janmaki.mqrimo.invisible_armor;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
+public class ClickEvent implements Listener {
+
+    @EventHandler
+    public void onPlayerClickInventory(InventoryClickEvent event){
+        if(event.getClickedInventory() instanceof PlayerInventory){
+            if(!(event.getWhoClicked() instanceof Player))
+                return;
+
+            Player player = (Player) event.getWhoClicked();
+            Boolean isArmorInvisible = Core.sectionalBoolean(player);
+            if(!isArmorInvisible){
+                return;
+            }
+            if(event.getSlotType() == InventoryType.SlotType.ARMOR) {
+                if (event.getClick() == ClickType.SHIFT_LEFT || event.getClick() == ClickType.SHIFT_RIGHT){
+                    if(event.getCurrentItem() != null) {
+                        player.sendMessage(Main.prefix + ChatColor.RED + "투명 갑옷 활성화 상태에서는 'SHIFT + 마우스 좌클릭'을 통한 방어구 탈의가 불가능합니다.");
+                        event.setCancelled(true);
+                        return;
+                    }
+                }
+                if(event.getClick() == ClickType.NUMBER_KEY){
+                    if(event.getCurrentItem() != null) {
+                        if(event.getInventory().getItem(event.getHotbarButton()) != null)
+                            player.sendMessage(Main.prefix + ChatColor.RED + "투명 갑옷 활성화 상태에서는 숫자 키를 통한 방어구 교체가 불가능합니다.");
+                        else
+                            player.sendMessage(Main.prefix + ChatColor.RED + "투명 갑옷 활성화 상태에서는 숫자 키를 통한 방어구 탈의가 불가능합니다.");
+
+                        event.setCancelled(true);
+                        return;
+                    }
+                }
+
+            }else if(event.getSlotType() == InventoryType.SlotType.CONTAINER || event.getSlotType() == InventoryType.SlotType.QUICKBAR){
+                if (event.getClick() == ClickType.SHIFT_LEFT || event.getClick() == ClickType.SHIFT_RIGHT){
+                    ItemStack item;
+                    boolean check = false;
+                    if(event.getCurrentItem().getType().name().contains("BOOT")) {
+                        item = ((PlayerInventory) event.getClickedInventory()).getArmorContents()[0];
+                        check = true;
+                    }
+
+                    else if(event.getCurrentItem().getType().name().contains("LEGGING")) {
+                        item = ((PlayerInventory) event.getClickedInventory()).getArmorContents()[1];
+                        check = true;
+                    }
+
+                    else if(event.getCurrentItem().getType().name().contains("CHESTPLATE")) {
+                        item = ((PlayerInventory) event.getClickedInventory()).getArmorContents()[2];
+                        check = true;
+                    }
+
+                    else if(event.getCurrentItem().getType().name().contains("HELMET")) {
+                        item = ((PlayerInventory) event.getClickedInventory()).getArmorContents()[3];
+                        check = true;
+                    }else{
+                        item = null;
+                        check = false;
+                    }
+
+                    if(check){
+                        if(item != null && !item.getType().name().equals("AIR")){
+                            event.setCancelled(true);
+                            player.sendMessage(Main.prefix + ChatColor.RED + "투명 갑옷 활성화 상태에서는 'SHIFT + 마우스 좌클릭'을 통한 방어구 교체가 불가능합니다.");
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
--- src/main/java/com/janmaki/mqrimo/invisible_armor/Main.java
+++ src/main/java/com/janmaki/mqrimo/invisible_armor/Main.java
@@ -3,6 +3,7 @@ package com.janmaki.mqrimo.invisible_armor;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -16,6 +17,7 @@ public class Main extends JavaPlugin {
     private static Map<Player, Map<String, Boolean>> map;
     private static Main instance;
     private static File saveFile;
+    public static String prefix = ChatColor.GOLD+"[ "+ChatColor.GREEN+""+ChatColor.BOLD+"Earth Realm "+ChatColor.GOLD+"]"+ChatColor.RESET+" ";
     @Override
     public void onEnable() {
         new Regularly(this);
@@ -29,6 +31,8 @@ public class Main extends JavaPlugin {
             }
         }
         Bukkit.getServer().getPluginManager().registerEvents(new JoinEvent(saveFile),this);
+        Bukkit.getServer().getPluginManager().registerEvents(new ClickEvent(),this);
+        Bukkit.getServer().getPluginManager().registerEvents(new InteractEvent(), this);
         new Core(saveFile);
         map = Core.get();
 
@@ -43,35 +47,43 @@ public class Main extends JavaPlugin {
             sender.sendMessage(ChatColor.RED+"*Cant be executed from the console");
             return true;
         }
+
         if (args.length < 1) {
-            if (sender.hasPermission("invArmor.*")) {
-                sendHelp((Player) sender);
+            if (!sender.hasPermission("invArmor.*") && !sender.hasPermission("invArmor.command.toggle") && !sender.hasPermission("invArmor.command.*")) {
+                ((Player) sender).sendMessage(prefix+ChatColor.RED+"아직 이 상품을 구매하지 않았거나, 상품을 사용할 권한을 부여받지 못했습니다.");
+            }else{
+                sendHelp((Player) sender, prefix);
             }
             return true;
         }
         Player player = (Player) sender;
         if (args[0].equalsIgnoreCase("toggle")) {
-            if (!(player.hasPermission("invArmor.*") || player.hasPermission("invArmor.command.toggle") || player.hasPermission("invArmor.command.*"))) {
-                player.sendMessage(ChatColor.RED+"아직 이 상품을 구매하지 않았거나, 상품을 사용할 권한을 부여받지 못했습니다.");
+            if (!player.hasPermission("invArmor.*") && !player.hasPermission("invArmor.command.toggle") && !player.hasPermission("invArmor.command.*")) {
+                player.sendMessage(prefix+ChatColor.RED+"아직 이 상품을 구매하지 않았거나, 상품을 사용할 권한을 부여받지 못했습니다.");
+                return true;
+
+            }
+            if(player.getGameMode() == GameMode.CREATIVE || player.getGameMode() == GameMode.SPECTATOR) {
+                player.sendMessage(prefix+ChatColor.RED + "투명 갑옷은 서바이벌에서만 사용 가능합니다. 현재 플레이어는 " + ChatColor.AQUA + player.getGameMode().name() + " " + ChatColor.RED + "상태입니다.");
                 return true;
             }
             if(Core.offInvArmor(player)){
-                player.sendMessage(ChatColor.GREEN+"갑옷 투명화를 비활성화 했습니다.");
+                player.sendMessage(prefix+ChatColor.GREEN+"갑옷 투명화를 비활성화 했습니다.");
             }else{
                 Core.invArmor(player);
-                player.sendMessage(ChatColor.GREEN+"갑옷 투명화를 활성화 했습니다.");
+                player.sendMessage(prefix+ChatColor.GREEN+"갑옷 투명화를 활성화 했습니다.");
             }
 
             return true;
         }
-        sendHelp(player);
+        sendHelp(player, prefix);
         return true;
     }
 
-    private void sendHelp(Player player) {
-        player.sendMessage(ChatColor.DARK_GREEN + "=---------------------------------=");
+    private void sendHelp(Player player, String prefix) {
+        player.sendMessage(ChatColor.DARK_GREEN + "=----------------- "+prefix+ChatColor.DARK_GREEN+"----------------=");
         player.sendMessage(ChatColor.GREEN + "/invArmor toggle" + ChatColor.DARK_GREEN + ": " + ChatColor.GREEN + "갑옷 투명화를 켜거나 끕니다.");
-        player.sendMessage(ChatColor.DARK_GREEN + "=---------------------------------=");
+        player.sendMessage(ChatColor.DARK_GREEN + "=-------------------------------------------------=");
     }
     public static Main getInstance(){
         return instance;
