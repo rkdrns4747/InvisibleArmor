0dda4f4236db1a323750a07b2832896779fda2d0
Pro_Snape <rkdrns4747@users.noreply.github.com>
2020-03-06 00:00:16 +0900

    Bug Fix

--- src/main/java/com/janmaki/mqrimo/invisible_armor/ClickEvent.java
+++ src/main/java/com/janmaki/mqrimo/invisible_armor/ClickEvent.java
@@ -15,6 +15,9 @@ public class ClickEvent implements Listener {
 
     @EventHandler
     public void onPlayerClickInventory(InventoryClickEvent event){
+        if(!(event.getView().getTopInventory().getType().name().contains("CRAFTING")))
+            return;
+
         if(event.getClickedInventory() instanceof PlayerInventory){
             if(!(event.getWhoClicked() instanceof Player))
                 return;
