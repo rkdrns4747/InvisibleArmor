502c596b07645497a0caea6d8f101c7992a116de
Pro_Snape <rkdrns4747@users.noreply.github.com>
2020-03-05 23:38:42 +0900

    1.15.2 - update

--- src/main/java/com/janmaki/mqrimo/invisible_armor/Core.java
+++ src/main/java/com/janmaki/mqrimo/invisible_armor/Core.java
@@ -1,13 +1,13 @@
 package com.janmaki.mqrimo.invisible_armor;
 
-import net.minecraft.server.v1_12_R1.EntityPlayer;
-import net.minecraft.server.v1_12_R1.EnumItemSlot;
-import net.minecraft.server.v1_12_R1.PacketPlayOutEntityEquipment;
+import net.minecraft.server.v1_15_R1.EntityPlayer;
+import net.minecraft.server.v1_15_R1.EnumItemSlot;
+import net.minecraft.server.v1_15_R1.PacketPlayOutEntityEquipment;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
-import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftItemStack;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_15_R1.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
--- src/main/java/com/janmaki/mqrimo/invisible_armor/InteractEvent.java
+++ src/main/java/com/janmaki/mqrimo/invisible_armor/InteractEvent.java
@@ -16,108 +16,53 @@ import java.util.List;
 public class InteractEvent implements Listener {
 
     @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event){
+    public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
 
         Boolean isArmorInvisible = Core.sectionalBoolean(player);
-        if(!isArmorInvisible)
+        if (!isArmorInvisible)
             return;
 
-        if(!event.getAction().name().toLowerCase().contains("right"))
+        if (!event.getAction().name().toLowerCase().contains("right"))
             return;
 
 
         ItemStack heldItem = event.getItem();
-        if(heldItem == null || heldItem.getType() == Material.AIR)
+        if (heldItem == null || heldItem.getType() == Material.AIR)
             return;
 
         boolean isInteractable = false;
-        if(event.getAction().name().toLowerCase().contains("block"))
-            isInteractable = interactables.contains(event.getClickedBlock().getType());
+        if (event.getAction().name().toLowerCase().contains("block"))
+            isInteractable = event.getClickedBlock().getType().isInteractable();
 
-        if(isInteractable)
+        if (isInteractable)
             return;
 
         ItemStack item;
         boolean check = false;
-        if(heldItem.getType().name().contains("BOOT")) {
+        if (heldItem.getType().name().contains("BOOT")) {
             item = player.getInventory().getArmorContents()[0];
             check = true;
-        }
-
-        else if(heldItem.getType().name().contains("LEGGING")) {
+        } else if (heldItem.getType().name().contains("LEGGING")) {
             item = player.getInventory().getArmorContents()[1];
             check = true;
-        }
-
-        else if(heldItem.getType().name().contains("CHESTPLATE")) {
+        } else if (heldItem.getType().name().contains("CHESTPLATE")) {
             item = player.getInventory().getArmorContents()[2];
             check = true;
-        }
-
-        else if(heldItem.getType().name().contains("HELMET")) {
+        } else if (heldItem.getType().name().contains("HELMET")) {
             item = player.getInventory().getArmorContents()[3];
             check = true;
-        }else{
+        } else {
             item = null;
             check = false;
         }
 
-        if(check){
-            if(item != null && !item.getType().name().equals("AIR")){
+        if (check) {
+            if (item != null && !item.getType().name().equals("AIR")) {
                 event.setCancelled(true);
                 player.sendMessage(Main.prefix + ChatColor.RED + "투명 갑옷 활성화 상태에서는 마우스 우클릭을 통한 방어구 교체가 불가능합니다.");
                 return;
             }
         }
     }
-    private static List<Material> interactables = Arrays.asList(
-            Material.ACACIA_DOOR,
-            Material.ACACIA_FENCE_GATE,
-            Material.ANVIL,
-            Material.BEACON,
-            Material.BED,
-            Material.BIRCH_DOOR,
-            Material.BIRCH_FENCE_GATE,
-            Material.BOAT,
-            Material.BOAT_ACACIA,
-            Material.BOAT_BIRCH,
-            Material.BOAT_DARK_OAK,
-            Material.BOAT_JUNGLE,
-            Material.BOAT_SPRUCE,
-            Material.BREWING_STAND,
-            Material.COMMAND,
-            Material.CHEST,
-            Material.DARK_OAK_DOOR,
-            Material.DARK_OAK_FENCE_GATE,
-            Material.DAYLIGHT_DETECTOR,
-            Material.DAYLIGHT_DETECTOR_INVERTED,
-            Material.DISPENSER,
-            Material.DROPPER,
-            Material.ENCHANTMENT_TABLE,
-            Material.ENDER_CHEST,
-            Material.FENCE_GATE,
-            Material.WORKBENCH,
-            Material.FURNACE,
-            Material.HOPPER,
-            Material.HOPPER_MINECART,
-            Material.ITEM_FRAME,
-            Material.JUNGLE_DOOR,
-            Material.JUNGLE_FENCE_GATE,
-            Material.LEVER,
-            Material.MINECART,
-            Material.NOTE_BLOCK,
-            Material.POWERED_MINECART,
-            Material.REDSTONE_COMPARATOR,
-            Material.REDSTONE_COMPARATOR_OFF,
-            Material.REDSTONE_COMPARATOR_ON,
-            Material.SIGN,
-            Material.SIGN_POST,
-            Material.STORAGE_MINECART,
-            Material.TRAP_DOOR,
-            Material.TRAPPED_CHEST,
-            Material.WALL_SIGN,
-            Material.WOOD_BUTTON,
-            Material.WOOD_DOOR);
 }
-
